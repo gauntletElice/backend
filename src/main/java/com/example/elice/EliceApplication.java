7d062fb2d28b1e3e639a8afb63519cdac901c357
JeongHyeon Jo <cisxo@naver.com>
2025-02-27 07:04:58 +0900

    feature(batch): ScheduledFetch for FortuneCookie - EliceApplication: 프로그램 시작 후 Scheduled 미리 시작시킴. - FortuneCookieScheduler: 비동기로 api 전송 시키고 db에 저장 오전 9시마다 업데이트

--- src/main/java/com/example/elice/EliceApplication.java
+++ src/main/java/com/example/elice/EliceApplication.java
@@ -1,13 +1,28 @@
 package com.example.elice;
 
+import com.example.elice.common.scheduled.FortuneCookieScheduler;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class EliceApplication {
+
+	private final FortuneCookieScheduler fortuneCookieScheduler;
+	@Autowired
+	public EliceApplication(FortuneCookieScheduler fortuneCookieScheduler) {
+		this.fortuneCookieScheduler = fortuneCookieScheduler;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(EliceApplication.class, args);
 	}
 
+	@PostConstruct
+	public void init() {
+		fortuneCookieScheduler.fetchFortuneCookie();
+	}
 }
