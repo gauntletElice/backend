4c4c376d404165a161042fe9739fb2df85f1ef6b
JeongHyeon Jo <cisxo@naver.com>
2025-02-28 04:28:40 +0900

    FEAT(domain): 집중시간 도메인 dto 필드 추가

--- src/main/java/com/example/elice/focus/domain/Focus.java
+++ src/main/java/com/example/elice/focus/domain/Focus.java
@@ -15,6 +15,8 @@ public class Focus {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    private Long memberId;
+
     private String tasks;
 
     private String targetTime;
