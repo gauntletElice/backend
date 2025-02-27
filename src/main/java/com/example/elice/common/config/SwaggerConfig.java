2f901346d51b43269058d4b3a23b81e909bb3e4b
JeongHyeon Jo <cisxo@naver.com>
2025-02-28 02:50:37 +0900

    FIX: SwaggerConfig cors err

--- src/main/java/com/example/elice/common/config/SwaggerConfig.java
+++ src/main/java/com/example/elice/common/config/SwaggerConfig.java
@@ -23,7 +23,6 @@ public class SwaggerConfig {
                         new SecurityScheme()
                                 .name(SECURITY_SCHEME_NAME)
                                 .type(SecurityScheme.Type.HTTP)
-                                .scheme("bearer")
                                 .bearerFormat("JWT"));
 
         return new OpenAPI()
