61cd4638dd51fd35df6fd10be4c7a98eee52a403
JeongHyeon Jo <cisxo@naver.com>
2025-03-01 00:15:03 +0900

    FIX(redis): redis 인식 에러

--- src/main/java/com/example/elice/common/config/RedisConfig.java
+++ src/main/java/com/example/elice/common/config/RedisConfig.java
@@ -11,7 +11,7 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
-@EnableRedisRepositories
+@EnableRedisRepositories(basePackages = "com.example.elice.common.auth.repository")
 @Configuration
 public class RedisConfig {
 
